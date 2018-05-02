8c55e0e979ced0a9a747ccd57dc2e1be58e1b212
desireetorrente <desiree.torrente@gmail.com>
2018-05-02 20:21:25 +0200

    Javadoc

--- Maraton/src/servicio/Validate.java
+++ Maraton/src/servicio/Validate.java
@@ -3,10 +3,23 @@ package servicio;
 import Modelo.Usuarios;
 import persistencia.UsuariosHelper;
 
+/**
+ * Clase para validar los usuarios que se conectan a la 
+ * aplicación web
+ * @author admin
+ *@version 02/05/2018
+ */
 public class Validate {
 
 	private static UsuariosHelper usuario = new UsuariosHelper(); 
 	
+	/**
+	 * Método estático que comprueba si el usuario
+	 * está regstrado y si la contraseña introducida es correcta
+	 * @param dni DNI del usuario que quiere acceder a la aplicación
+	 * @param pass Contraseña de acceso del usuario
+	 * @return Booleano que indica si el proceso de autenticación ha sido correcto o no
+	 */
 	public static boolean checkUser(int dni, String pass) {
 		
 		boolean correcto = false;
@@ -25,6 +38,11 @@ public class Validate {
 		return correcto;
 	}
 	
+	/**
+	 * Método para buscar el usuario en la BBDD
+	 * @param dni DNI del usuario que quiere acceder a la web
+	 * @return Objeto usuario con ese DNI
+	 */
 	public static Usuarios getUser(int dni) {
 		return usuario.search(dni);
 	}
