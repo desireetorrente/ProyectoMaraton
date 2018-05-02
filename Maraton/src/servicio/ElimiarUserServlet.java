139e9461732e672d197468ef340d0f93fb8409db
desireetorrente <desiree.torrente@gmail.com>
2018-05-02 19:27:45 +0200

    Javadoc

--- Maraton/src/servicio/ElimiarUserServlet.java
+++ Maraton/src/servicio/ElimiarUserServlet.java
@@ -2,25 +2,27 @@ package servicio;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import Modelo.Participantes;
 import persistencia.UsuariosHelper;
 
 /**
- * Servlet implementation class ElimiarUserServlet
+ * Servlet que implementa la clase ElimiarUserServlet
+ * Esta clase es el controlador que gestiona las peticiones de eliminar
+ * usuarios que llegan desde la Web
+ * Esta clase heresa de HttpServlet
+ * @author admin
+ * @version 02/05/2018
  */
 @WebServlet("/ElimiarUserServlet")
 public class ElimiarUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
+     * Contructor por defecto
      * @see HttpServlet#HttpServlet()
      */
     public ElimiarUserServlet() {
@@ -28,13 +30,21 @@ public class ElimiarUserServlet extends HttpServlet {
     }
 
 	/**
+	 * Método que gestiona las peticiones Get que se generan en el cliente
+	 * En este caso no se gestionan peticiones de este tipo 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
+	 * Método paras las petiones Post que se generan en el cliente
+	 * En este caso entra la petición de eliminar un usuario totalmente, tanto 
+	 * si sólo esta registrado como, si es participante en alguna carrera
+	 * Este método recoge los parámetros introducidos en el formulario de eliminar de la zona
+	 * de usuarios, se comprueba que el usuario existe en la BBDD y la contraseña introducida
+	 * es correcta y, si todo es correcto se elimina el usuario
+	 * El resultado de este proceso es mostrador en el navegador al usuario
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
