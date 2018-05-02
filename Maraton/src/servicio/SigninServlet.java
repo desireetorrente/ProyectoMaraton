e62f6a18445f7fcfcb15f86130c629ede2f795a4
desireetorrente <desiree.torrente@gmail.com>
2018-05-02 20:16:06 +0200

    Javadoc y eliminar svweb

--- Maraton/src/servicio/SigninServlet.java
+++ Maraton/src/servicio/SigninServlet.java
@@ -2,19 +2,20 @@ package servicio;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import controlador.ControladorUsuario;
 
 /**
- * Servlet implementation class SigninServlet
+ * Servlet que implementa la clase SigninServlet
+ * Esta clase es el controlador que gestiona las peticiones
+ * de registrar a un nuevo usuario en la aplicación
+ * Esta clase hereda de HttpServlet
+ * @author admin
+ * @version 02/05/2018
  */
 @WebServlet("/SigninServlet")
 public class SigninServlet extends HttpServlet {
@@ -22,6 +23,7 @@ public class SigninServlet extends HttpServlet {
        
 	private ControladorUsuario user = new ControladorUsuario();
     /**
+     * Constructor por defecto
      * @see HttpServlet#HttpServlet()
      */
     public SigninServlet() {
@@ -29,13 +31,20 @@ public class SigninServlet extends HttpServlet {
     }
 
 	/**
+	 * Método que gestiona las peticiones Get que se generan en el cliente
+	 * En este caso no se gestionan peticiones de este tipo 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
+	/**Método para las peticiones Post que se generan en el cliente
+	 * En este caso se genera la petición de un nuevo usuario de registrarse
+	 * El usuario mete sus datos en el formulario de inscripción y comprueba si el
+	 * usuario ya existe o no en la BBDD.
+	 * En el caso de que no exista, se procede a su registro y se informa al usuario
+	 * Si durante este proceso se produce un error se avisa al usuario por pantalla
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
