ea3eaa02593550b3bdcda3f24386c8a057fc96f3
desireetorrente <desiree.torrente@gmail.com>
2018-05-02 19:56:02 +0200

    javadoc

--- Maraton/src/servicio/ListarCarreras.java
+++ Maraton/src/servicio/ListarCarreras.java
@@ -3,25 +3,30 @@ package servicio;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import Modelo.Carrera;
-import Modelo.Participantes;
 import persistencia.CarrerasHelper;
 
 /**
- * Servlet implementation class ListarCarreras
+ * Servlet que implementa la clase ListarCarreras
+ * Esta clase es el controlador que gestiona las peticiones
+ * de los usuarios de ver las carreras que hay disponibles en este
+ * momento. Dichas peticiones se realizan a través del navegador
+ * Esta clase hereda de HttpServlet
+ * @author admin
+ * @version 02/05/2018 
  */
 @WebServlet("/ListarCarreras")
 public class ListarCarreras extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private CarrerasHelper ch = new CarrerasHelper();
+    
     /**
+     * Constructor por defecto
      * @see HttpServlet#HttpServlet()
      */
     public ListarCarreras() {
@@ -29,6 +34,8 @@ public class ListarCarreras extends HttpServlet {
     }
 
 	/**
+	 * Método que gestiona las peticiones Get que se generan en el cliente
+	 * En este caso no se gestionan peticiones de este tipo 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -36,6 +43,10 @@ public class ListarCarreras extends HttpServlet {
 	}
 
 	/**
+	 * Método para las peticiones Post que se generan en el cliente
+	 * En este caso entra la petición de listar las carreras disponibles
+	 * Se recibe una lista con las carreras disponibles en la BBDD y se imprime la respuesta
+	 * que saldrá por el navegador
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
